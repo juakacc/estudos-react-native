133d4fa44efb68ca6cd2ed811a9bbe412c0ffd52
Joaquim <juakacc@gmail.com>
2019-08-27 15:19:03 -0300

    Adicionando mask completa

--- TestCamera/AppC.js
+++ TestCamera/AppC.js
@@ -1,7 +1,16 @@
 import React from 'react'
-import {Dimensions, StyleSheet, View} from 'react-native'
+import {
+    CameraRoll,
+    Dimensions, 
+    StyleSheet, 
+    View, 
+    Image, 
+    Text, 
+    TouchableOpacity
+} from 'react-native'
 import {RNCamera} from 'react-native-camera'
 import CameraOverlayC from './CameraOverlayC'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class AppC extends React.Component {
     render() {
@@ -9,31 +18,61 @@ export default class AppC extends React.Component {
             <View style={styles.container}>
                 <RNCamera
                     style={styles.camera}
-                    ref={ref => {this.camera = ref}}
+                    ref={ref => {this.camera = ref;}}
                     type={RNCamera.Constants.Type.back} />
                 <CameraOverlayC />
+                <View style={styles.cameraElements}>
+                    <View style={{alignItems: 'center'}}>
+                        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.buttom}>
+                            <Icon name="camera" size={40} color='#342' />                
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         )
     }
+
+    takePicture = async() => {
+        // if (this.camera) {
+        //   const options = { quality: 0.5, base64: true }
+        //   const data = await this.camera.takePictureAsync(options).then(data => {
+        //     CameraRoll.saveToCameraRoll(data.uri)
+        //     console.log(data)
+        //   })
+        //   console.log(data.uri)
+        // }
+      }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#f5fcff',
-
-        // flexDirection: 'column',
-        // backgroundColor: 'black',
     },
     camera: {
         position: 'absolute',
         flex: 1,
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
-
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    }
+    },
+    cameraElements: {
+        zIndex: 2,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingHorizontal: 20
+    },
+    smartphoneImage: {
+        width: 50,
+        resizeMode: 'contain'
+    },
+    buttom: {
+        marginBottom: 20,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: '#5d4'
+    },
 })
--- TestCamera/CameraOverlayC.js
+++ TestCamera/CameraOverlayC.js
@@ -1,27 +1,32 @@
 import React from 'react'
-import {Dimensions, View, Text} from 'react-native'
+import {Dimensions, View} from 'react-native'
 import Svg, {Circle, Defs, Rect, Mask} from 'react-native-svg'
 
-export default () => {
-    const {height, width} = Dimensions.get('window')
+const CameraOverlayC = () => {
+    const height = Dimensions.get('window').height
+    const width = Dimensions.get('window').width
     const circleRadius = width / 2.5
     const viewBox = `0 0 ${width} ${height}`
 
     return (
-        <View aspectRadio={1}>
-            <Svg height={height} width={width}>
+        <View aspectRatio={1}>
+            <Svg 
+                height={height} 
+                viewBox={viewBox}>
                 <Defs>
-                    <Mask id='mask'>
-                        <Rect height={height} width={width} fill='#fff' />
-                        <Circle r={circleRadius} cx={width/2} cy={height/2} fill='#000' />
+                    <Mask id="mask">
+                        <Rect height={height} width={width} fill='#ffffff' />
+                        <Circle r={circleRadius} cx={width/2} cy={height/2} />
                     </Mask>
                 </Defs>
                 <Rect
                     height={height}
                     width={width}
-                    fill='#ffffff'
-                    mask='url(#mask)' />
+                    fill='#fff'
+                    mask="url(#mask)" />
             </Svg>
         </View>
     )
 }
+
+export default CameraOverlayC
